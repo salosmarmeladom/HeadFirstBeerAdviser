8444c8c61b107df82a83da23c83e1e949518f5d7
salosmarmeladom <pollyklaps@outlook.com>
2022-03-08 22:34:49 +0300

    Initial commit

--- app/src/main/java/laba/project/beeradviser/FindBeerActivity.java
+++ app/src/main/java/laba/project/beeradviser/FindBeerActivity.java
@@ -3,10 +3,7 @@ package laba.project.beeradviser;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-<<<<<<< HEAD
-=======
 import android.widget.Spinner;
->>>>>>> 7c15d80 (Initial commit)
 import android.widget.TextView;
 
 public class FindBeerActivity extends Activity {
@@ -22,14 +19,10 @@ public class FindBeerActivity extends Activity {
     //called when the user clicks the button
     public void onClickFindBeer (View view){
         TextView brands = (TextView) findViewById(R.id.brands);
-<<<<<<< HEAD
-        brands.setText("Gottle of geer");
-=======
         Spinner color = (Spinner) findViewById(R.id.color);
 
         String beerType = String.valueOf(color.getSelectedItem());
         brands.setText(beerType);
->>>>>>> 7c15d80 (Initial commit)
     }
 
 }
